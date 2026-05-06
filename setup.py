d1723ba9778d3412d8269f8dddd6028d14a500a3
Pathos <29135741+Pathos14489@users.noreply.github.com>
2026-05-06 14:47:16 -0700

    updated requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='xtts_api_server',
-    version='0.1.0',
+    version='0.1.1',
     description='Mantella XTTS API Server',
     author='xyz',
     author_email='your.email@example.com',
     packages=find_packages(),
     install_requires=[
         'PyAudio==0.2.14',
-        'requests==2.31.0',
+        'requests==2.32.0',
         'pyttsx3==2.90',
         'stream2sentence==0.2.2',
         'fastapi>=0.104.1',
